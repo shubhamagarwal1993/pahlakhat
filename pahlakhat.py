8afe30bfb71f5fdfcd5c5b72aace9fe851f59b44
Ubuntu <ubuntu@ip-172-31-32-249.us-west-2.compute.internal>
2017-06-24 13:52:38 +0000

    updated inedx and get functions

--- pahlakhat.py
+++ pahlakhat.py
@@ -1,4 +1,20 @@
-from flask import Flask, jsonify, abort, make_response, request
+from flask import Flask, jsonify, abort, make_response, request, url_for
+
+# setting up authentication
+from flask.ext.httpauth import HTTPBasicAuth
+auth = HTTPBasicAuth()
+
+# Setting one user to access
+@auth.get_password
+def get_password(username):
+    if username == 'nam08':
+        return 'panridhu'
+    return None
+
+# Setting msg for non-authorized user
+@auth.error_handler
+def unauthorized():
+    return make_response(jsonify({'error': 'Unauthorized access'}), 401)
 
 # this line is to read from gunicorn. do not change it
 from werkzeug.contrib.fixers import ProxyFix
@@ -22,6 +38,7 @@ tasks = [
 
 # Path for GET
 @app.route('/tasks', methods=['GET'])
+@auth.login_required
 def get_tasks():
     return jsonify({'tasks': tasks})
 
@@ -31,7 +48,7 @@ def get_task(task_id):
     task = [task for task in tasks if task['id'] == task_id]
     if len(task) == 0:
         abort(404)
-    return jsonify({'task': task[0]})
+    return jsonify({'task': [make_public_task(task) for task in tasks]})
 
 # This is custom message for handling 404 error
 @app.errorhandler(404)
@@ -69,7 +86,7 @@ def update_task(task_id):
     task[0]['title'] = request.json.get('title', task[0]['title'])
     task[0]['description'] = request.json.get('description', task[0]['description'])
     task[0]['done'] = request.json.get('done', task[0]['done'])
-    return jsonify({'task': task[0]})
+    return jsonify({'task': [make_public_task(task) for task in tasks]})
 
 # Path for DELETE
 @app.route('/tasks/<int:task_id>', methods=['DELETE'])
@@ -79,6 +96,16 @@ def delete_task(task_id):
         abort(404)
     tasks.remove(task[0])
     return jsonify({'result': True})
+
+# this will make the full URI that controls the task
+def make_public_task(task):
+    new_task = {}
+    for field in task:
+        if field == 'id':
+            new_task['uri'] = url_for('get_task', task_id=task['id'], _external=True)
+        else:
+            new_task[field] = task[field]
+    return new_task
 
 # this line is to read from gunicorn. do not change it
 app.wsgi_app = ProxyFix(app.wsgi_app)
